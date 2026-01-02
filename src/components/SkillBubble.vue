269ad9990facf16d8f94e6eddeff087ff665fd0c
Fristaylo <andru_2004@mail.ru>
2026-01-02 21:58:02 +0400

    Fix skill bubble hover effect | Add transition duration

--- src/components/SkillBubble.vue
+++ src/components/SkillBubble.vue
@@ -1,6 +1,5 @@
 <script setup lang="ts">
 import { skills } from '../data/skill';
-
 const props = defineProps<{
 	id: string;
 }>();
@@ -12,8 +11,13 @@ const skill = skills.find((s) => s.id === props.id);
 	<NuxtLink :to="{ name: 'skills-id', params: { id: props.id } }">
 		<div
 			v-if="skill"
-			class="flex px-2 py-0.5 gap-2 border-3 rounded-2xl items-center"
-			:style="{ borderColor: skill.color }"
+			:style="{
+				'--skill-color': skill.color,
+				'--skill-color-hover': `color-mix(in srgb, var(--skill-color) 75%, black)`
+			}"
+			class="flex px-2 py-0.5 gap-2 border-3 rounded-2xl items-center
+			border-[color:var(--skill-color)] transition-colors duration-300
+			hover:border-[color:var(--skill-color-hover)]"
 		>
 			<div v-html="skill.icon"></div>
 			<div class="font-semibold text-md">
